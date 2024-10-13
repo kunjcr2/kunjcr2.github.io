b0704dc26b4c714c8c2bd4726360a6b3312b4d3e
Kunj Shah <159096079+kunjcr2@users.noreply.github.com>
2024-10-13 13:04:35 -0700

    Update portfolioWeb-script.js

--- portfolioWeb-script.js
+++ portfolioWeb-script.js
@@ -1,16 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     const certificates = [
-        'Data Science Certificate',
-        'Full-Stack Web Development Certificate',
-        'Machine Learning Certificate',
-        'Python Programming Certificate'
+        'Programming for Everybody (Getting Started with Python) – University of Michigan',
+        'Python Data Structures – University of Michigan',
+        'Introduction to Generative AI – Google Cloud',
+        'Crash course on Python – Google',
+        'Introduction to Web Development – UC Davis',
+        'Calculus through data and modelling: Series and integration – Johns Hopkins University',
+        'Calculus through data and modelling: Techniques of integration – Johns Hopkins University',
+        'Calculus through data and modelling: Integration Applications – Johns Hopkins University',
+        'Calculus through data and modelling: Vector Calculus – Johns Hopkins University',
+        'Understanding Einstein: Special theory of relativity – Stanford University',
+        'Introduction to complex analysis – Wesleyan University',
+        'Understanding Basic SQL Syntax – Coursera Project Network',
+        'C++ Basics: Selection and Iteration – Codec',
+        'Building a Text-based Bank – Coursera Project Network',
+        'Create a Supermarket app using Java OOP – Coursera Project Network',
+        'Python 101: Develop Your First Python Program – Coursera Project Network'
     ];
 
     const certificatesList = document.getElementById('certificates-list');
 
-    certificates.forEach(certificate => {
-        let li = document.createElement('li');
-        li.textContent = certificate;
-        certificatesList.appendChild(li);
+    certificates.forEach(function(cert) {
+        const listItem = document.createElement('li');
+        listItem.textContent = cert;
+        certificatesList.appendChild(listItem);
     });
 });
